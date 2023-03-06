7263ce484d1e062d18823147d398fabb2bb822b1
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-03-05 22:23:55 -0300

    Create Desafio3.js

---
+++ DIO/Codecamp-1s/Desafio3.js
@@ -0,0 +1,8 @@
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+let id = parseInt(gets());
+
+//TODO: Print no console se a semente deve ser plantada ou se o buraco deve
+//avançar ou retroceder X metros
