63933488e477bd1e95408192aab984f491a78d30
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-03-21 16:05:44 -0300

    Update ValidateCnpj.js

--- utils/ValidateCnpj.js
+++ utils/ValidateCnpj.js
@@ -1,18 +1,56 @@
-function validatCNPJ(cnpj){
-    const finalCnpj = cnpj;
-    const multiplyArray = [5,4,3,2,9,8,7,6,5,4,3,2];
-    const secondArray = [6,5,4,3,2,9,8,7,6,5,4,3,2];
-    let digitoVerificador = 0;
-    //1 retirar os dois ultimos numeros
-    //2 multiplicar pelo primeiro array
-    //3 o resultado é dividido por 11
-    //4 pegar o resto da divisao
-    //5 Se o resto for menor que 2, o digito será 0,
-    //caso contrario pegar o 11 e subtrair pelo resto
-    //que resultara no digito
-    //6 no segundo caso multiplicar o cnpj + novo digito pelo segundo array
-    //7 dividir novamente por 11
-    //8 se o resto desta divisao for menor que 2 considerar 0
-    //Caso seja maior que 2 pegar o inteiro da divisao e subtrair por 11
-    //9 o resultado da subtração por 11 sera o ultimo digito
+function validatCNPJ(cnpj) {
+    const multiplyArray = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    let resto = 0;
+
+    if (cnpj.length == 14) {
+        const finalCnpj = parseInt(cnpj.join(''));
+        let digito;
+        let soma = 0;
+
+        cnpj.pop();
+        cnpj.pop();
+
+        for (let i = 0; i < cnpj.length; i++) {
+            soma += multiplyArray[i] * cnpj[i];
+        }
+        resto = soma % 11;
+
+        if (resto >= 2) {
+            digito = 11 - resto;
+        } else {
+            digito = 0;
+        }
+
+        cnpj.push(digito);
+
+        multiplyArray.unshift(6);
+        soma = 0;
+
+        for (let i = 0; i < cnpj.length; i++) {
+            soma += multiplyArray[i] * cnpj[i];
+        }
+        resto = soma % 11;
+
+        if (resto >= 2) {
+            digito = 11 - resto;
+        } else {
+            digito = 0;
+        }
+
+        cnpj.push(digito);
+
+        const reformatCnpj = parseInt(cnpj.join(''));
+
+        if (reformatCnpj == finalCnpj) {
+            console.log("CPF VALIDO!");
+            return true;
+        } else {
+            console.log("CPF INVALIDO!");
+        }
+    }
 }
+
+primeiro = '11111111000111';
+
+myArr = primeiro.toString().split('').map(num => parseInt(num));
+validatCNPJ(myArr);
